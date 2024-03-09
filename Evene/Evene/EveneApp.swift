ad717386185a0f8ce6fbc51bced3caff4242266e
Pabitapun23 <pabita.pun23@gmail.com>
2024-03-09 00:29:26 -0500

    initial setup

---
+++ Evene/Evene/EveneApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveneApp.swift
+//  Evene
+//
+//  Created by Pabita Pun on 2024-03-09.
+//
+
+import SwiftUI
+
+@main
+struct EveneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
